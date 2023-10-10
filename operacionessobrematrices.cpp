b6a486e1a7ac57c9496b216b423094c2ea0d1755
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-10-10 10:54:22 -0600

    Update operacionessobrematrices.cpp

--- operacionessobrematrices.cpp
+++ operacionessobrematrices.cpp
@@ -7,114 +7,126 @@ int main()
     int n, i, j, opcion, suma = 0; 
     srand(time(NULL));
 
-    printf("ingresa la longitud de la matriz:\n");
-    scanf("%d", &n); 
-    int A[n][n];
-    int B[n][n]; 
+    do {
+        printf("Ingresa la longitud de la matriz:\n");
+        scanf("%d", &n); 
+        int A[n][n];
+        int B[n][n]; 
 
-    printf("matriz A:\n");
-    for (i = 0; i < n; i++) 
-    {
-        for (j = 0; j < n; j++) 
+        printf("Matriz A:\n");
+        for (i = 0; i < n; i++) 
         {
-            A[i][j] = rand() % 9 + 1; 
-            printf("%d ", A[i][j]);
+            for (j = 0; j < n; j++) 
+            {
+                A[i][j] = rand() % 9 + 1; 
+                printf("%d ", A[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
 
-    printf("matriz B:\n");
-    for (i = 0; i < n; i++) 
-    {
-        for (j = 0; j < n; j++) 
+        printf("Matriz B:\n");
+        for (i = 0; i < n; i++) 
         {
-            B[i][j] = rand() % 9 + 1;
-            printf("%d ", B[i][j]);
+            for (j = 0; j < n; j++) 
+            {
+                B[i][j] = rand() % 9 + 1;
+                printf("%d ", B[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
-    
-    printf("que operacion quieres realizar?\n");
-    printf("1: Suma del triangular superior de A\n");
-    printf("2: Suma de A + B\n");
-    printf("3: Resta A - B\n");
-    printf("4: Multiplicación A x B\n");
-    printf("5: diagonal de A\n"); 
-    printf("ingresa la opcion o escribe otro numero para terminar: ");
-    scanf("%d", &opcion);
-    
-    switch(opcion)
-	{
-        case 1:
-            suma = 0;
-    		for (i = 0; i < n; i++) 
-    			{
-        			for (j = i + 1; j < n; j++) 
-        				{
-            		suma += A[i][j];
-        				}
-    			}
-    	printf("suma del triangular superior de A: %d\n", suma);
-            break;
-            
-        case 2:
-            printf("resultado de la suma de A + B:\n");
-            for (i = 0; i < n; i++) 
-            {
-                for (j = 0; j < n; j++) 
+        
+        printf("que operacion quieres realizar?\n");
+        printf("1: Suma del triangular superior de A\n");
+        printf("2: Suma de A + B\n");
+        printf("3: Resta A - B\n");
+        printf("4: MultiplicaciÃ³n A x B\n");
+        printf("5: Diagonal de A\n"); 
+        printf("0: Terminar el programa\n");
+        printf("Ingresa la opcion: ");
+        scanf("%d", &opcion);
+        
+        switch(opcion)
+    	{
+            case 1:
+                suma = 0;
+        		for (i = 0; i < n; i++) 
+        			{
+            			for (j = i + 1; j < n; j++) 
+            				{
+                		suma += A[i][j];
+            				}
+        			}
+            	printf("suma del triangular superior de A: %d\n", suma);
+                break;
+                
+            case 2:
+                printf("suma de A + B:\n");
+                for (i = 0; i < n; i++) 
                 {
-                    int resultado = A[i][j] + B[i][j];
-                    printf("%d ", resultado);
-                }
-                printf("\n");
-            }
-            break;
-            
-        case 3:
-            printf("resultado de la resta A - B:\n");
-            for (i = 0; i < n; i++) 
-            {
-                for (j = 0; j < n; j++) 
-                {
-                    int resultado = A[i][j] - B[i][j];
-                    printf("%d ", resultado);
-                }
-                printf("\n");
-            }
-            break;
-            
-        case 4:
-            printf("resultado de la multiplicación A x B:\n");
-            int C[n][n];
-            for (i = 0; i < n; i++) 
-            {
-                for (j = 0; j < n; j++) 
-                {
-                    C[i][j] = 0;
-                    for (int k = 0; k < n; k++) 
+                    for (j = 0; j < n; j++) 
                     {
-                        C[i][j] += A[i][k] * B[k][j];
+                        int resultado = A[i][j] + B[i][j];
+                        printf("%d ", resultado);
                     }
-                    printf("%d ", C[i][j]);
+                    printf("\n");
                 }
-                printf("\n");
-            }
-            break;
-            
-        case 5:
-            suma = 0;
-            for (i = 0; i < n; i++) 
-            {
-                suma += A[i][i]; 
-            }
-            printf("diagonal: %d\n", suma);
-            break;
-            
-        default:
-            printf("\nacabando programa:)))))\n");
-            break;
-    }
-    
+                break;
+                
+            case 3:
+                printf("resta A - B:\n");
+                for (i = 0; i < n; i++) 
+                {
+                    for (j = 0; j < n; j++) 
+                    {
+                        int resultado = A[i][j] - B[i][j];
+                        printf("%d ", resultado);
+                    }
+                    printf("\n");
+                }
+                break;
+                
+            case 4:
+                printf("multiplicacion A x B:\n");
+                int C[n][n];
+                for (i = 0; i < n; i++) 
+                {
+                    for (j = 0; j < n; j++) 
+                    {
+                        C[i][j] = 0;
+                        for (int k = 0; k < n; k++) 
+                        {
+                            C[i][j] += A[i][k] * B[k][j];
+                        }
+                        printf("%d ", C[i][j]);
+                    }
+                    printf("\n");
+                }
+                break;
+                
+            case 5:
+                suma = 0;
+                for (i = 0; i < n; i++) 
+                {
+                    suma += A[i][i]; 
+                }
+                printf("suma de la diagonal de A: %d\n", suma);
+                break;
+                
+            case 0:
+                printf("\nterminando el programa\n");
+                break;
+                
+            default:
+                printf("\nopcion no valida. Por favor, ingresa una opciÃ³n valida.\n");
+                break;
+        }
+        
+        if (opcion != 0) {
+            printf("Quieres repetir el proceso (1) o terminar (0)? ");
+            scanf("%d", &opcion);
+        }
+        
+    } while (opcion == 1);
+    printf("adioooos:)");
     return 0;
 }
-
