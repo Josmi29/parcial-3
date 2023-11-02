aac1cc994158e8a67d0cd339fa8ced889c96918a
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-11-02 10:58:41 -0600

    Update ordenamientodedatos.cpp

--- ordenamientodedatos.cpp
+++ ordenamientodedatos.cpp
@@ -3,7 +3,6 @@
 #include <time.h>
 //jose miguel ruiz marquez
 //180200
-//funcion que use para el minimo
 int findMin(int lista[], int n) 
 {
     int min = lista[0];
@@ -16,10 +15,12 @@ int findMin(int lista[], int n)
     }
     return min;
 }
-//funcion que use para el maximo
-int findMax(int lista[], int n) {
+
+int findMax(int lista[], int n) 
+{
     int max = lista[0];
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) 
+	{
         if (lista[i] > max) 
 		{
             max = lista[i];
@@ -27,37 +28,36 @@ int findMax(int lista[], int n) {
     }
     return max;
 }
-//funcion que use para la media
-float calcularMedia(int lista[], int n) {
+
+float calcularMedia(int lista[], int n) 
+{
     float suma = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+	{
         suma += lista[i];
     }
     return suma / n;
 }
-//funcion que use para la moda
+
 int calcularModa(int lista[], int n) 
 {
     int moda = lista[0];
     int conteomaximo = 0;
 
-    for (int i = 0; i < n; i++) 
-	{
+    for (int i = 0; i < n; i++) {
         int conteo = 0;
-        for (int j = 0; j < n; j++) 
-		{
+        for (int j = 0; j < n; j++) {
             if (lista[j] == lista[i])
                 conteo++;
         }
-        if (conteo > conteomaximo) 
-		{
+        if (conteo > conteomaximo) {
             conteomaximo = conteo;
             moda = lista[i];
         }
     }
     return moda;
 }
-//funcion que use para la mediana
+
 float calcularMediana(int lista[], int n) {
     if (n % 2 == 0) {
         return (float)(lista[n / 2 - 1] + lista[n / 2]) / 2.0;
@@ -82,7 +82,7 @@ int main() {
         printf(" %d ", lista[i]);
     }
     printf("\n");
-//aca uso el ordenamiento por insercion
+
     for (i = 0; i < n; i++) {
         numeroActual = lista[i];
         int posAnterior = i - 1;
@@ -99,7 +99,7 @@ int main() {
     for (i = 0; i < n; i++) {
         printf(" %d ", lista[i]);
     }
-//mando a llamar las funciones
+
     int minimo = findMin(lista, n);
     int maximo = findMax(lista, n);
     float media = calcularMedia(lista, n);
@@ -111,13 +111,19 @@ int main() {
     printf("media: %.2f\n", media);
     printf("moda: %d\n", moda);
     printf("mediana: %.2f\n", mediana);
-//aca genero el histograma
+
     printf("\nhistograma :\n");
+    int frecuencia[30] = {0}; 
+
     for (i = 0; i < n; i++) 
 	{
-        float probabilidad = 100 * (1.0 / n); 
-        printf("%d: ", lista[i]);
-        for (j = 0; j < probabilidad * lista[i]; j++) 
+        frecuencia[lista[i] - 1]++;
+    }
+
+    for (i = 0; i < 30; i++) {
+        float probabilidad = 100 * ((float)frecuencia[i] / n);
+        printf("%2d: ", i + 1);
+        for (j = 0; j < probabilidad; j++) 
 		{
             printf("*");
         }
@@ -126,4 +132,3 @@ int main() {
 
     return 0;
 }
-
